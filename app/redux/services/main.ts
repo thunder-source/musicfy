e2ac2fad5624014375162d02670bda898568e551
thunder-source <majhipraditya@gmail.com>
2024-05-16 02:35:22 +0530

    added env for different variables

--- app/redux/services/main.ts
+++ app/redux/services/main.ts
@@ -21,7 +21,10 @@ interface NewReleases {
 export const mainApi = createApi({
   reducerPath: 'mainApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000/api/',
+    baseUrl:
+      process.env.NODE_ENV == 'development'
+        ? 'http://localhost:3000/api/'
+        : 'https://music-api.pradityamanjhi.site/api/',
     // baseUrl: 'https://music-api.pradityamanjhi.site/api/',
   }),
   endpoints: (builder) => ({
